855008070bb02156ebda29c706cda9416787415d
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2023-09-22 21:48:59 -0300

    adding tailwindcss at the project

---
+++ BKM-EVENTOS/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/{main.tsx}"],
+  theme: {
+    colors:{
+      'purple':{
+        300: '#c4b5fd',
+        400: '#a78bfa',
+        500: '#8b5cf6',
+        600: '#4f46e5',
+        700: '#6d28d9',
+        800: '#5b21b6',
+        900: '#581c87',
+        950: '#2e1065',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
