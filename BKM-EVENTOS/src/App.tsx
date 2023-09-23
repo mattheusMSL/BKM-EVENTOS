e58248448e61827e68d11df068addc9aa1d885ad
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2023-09-22 22:52:23 -0300

    last changes

--- BKM-EVENTOS/src/App.tsx
+++ BKM-EVENTOS/src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header"
 function App() {
   return (
     <>
-      <h1 className="text-purple-600 flex justify-center align-middle">Hello World</h1>  
+      <h1 className="text-purple-600 flex justify-center align-middle text-6xl"> Hello World</h1>  
       <Header/>
     </>
   )
